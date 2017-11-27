740ddc1919a7af97ca9dcdaaa862b94fae9f05cc
ghyuk <jhyuk1492@gmail.com>
2017-11-28 02:29:03 +0900

    checkoutVO 생성 및 insertForm id, name수정

---
+++ src/main/java/com/project/main/checkIn/vo/CheckOutVO.java
@@ -0,0 +1,147 @@
+package com.project.main.checkIn.vo;
+
+public class CheckOutVO {
+
+	private String u_lname;
+	private String u_fname;
+	private String birth;
+	private String u_edate;
+	private String u_phone;
+	private String u_email;
+	
+	private String cardnum;
+	private String cardDateYear;
+	private String cardDateMonth;
+	private String securitycode;
+	private String cardfname;		
+	private String cardlname;
+	
+	private String billfname;
+	private String billlname;
+	private String comname;
+	
+	private String billemail;
+	private String billphone;
+	private String address;
+	private String postcode;
+	
+	public String getU_lname() {
+		return u_lname;
+	}
+	public void setU_lname(String u_lname) {
+		this.u_lname = u_lname;
+	}
+	public String getU_fname() {
+		return u_fname;
+	}
+	public void setU_fname(String u_fname) {
+		this.u_fname = u_fname;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getU_edate() {
+		return u_edate;
+	}
+	public void setU_edate(String u_edate) {
+		this.u_edate = u_edate;
+	}
+	public String getU_phone() {
+		return u_phone;
+	}
+	public void setU_phone(String u_phone) {
+		this.u_phone = u_phone;
+	}
+	public String getU_email() {
+		return u_email;
+	}
+	public void setU_email(String u_email) {
+		this.u_email = u_email;
+	}
+	public String getCardnum() {
+		return cardnum;
+	}
+	public void setCardnum(String cardnum) {
+		this.cardnum = cardnum;
+	}
+	public String getCardDateYear() {
+		return cardDateYear;
+	}
+	public void setCardDateYear(String cardDateYear) {
+		this.cardDateYear = cardDateYear;
+	}
+	public String getCardDateMonth() {
+		return cardDateMonth;
+	}
+	public void setCardDateMonth(String cardDateMonth) {
+		this.cardDateMonth = cardDateMonth;
+	}
+	public String getSecuritycode() {
+		return securitycode;
+	}
+	public void setSecuritycode(String securitycode) {
+		this.securitycode = securitycode;
+	}
+	public String getCardfname() {
+		return cardfname;
+	}
+	public void setCardfname(String cardfname) {
+		this.cardfname = cardfname;
+	}
+	public String getCardlname() {
+		return cardlname;
+	}
+	public void setCardlname(String cardlname) {
+		this.cardlname = cardlname;
+	}
+	public String getBillfname() {
+		return billfname;
+	}
+	public void setBillfname(String billfname) {
+		this.billfname = billfname;
+	}
+	public String getBilllname() {
+		return billlname;
+	}
+	public void setBilllname(String billlname) {
+		this.billlname = billlname;
+	}
+	public String getComname() {
+		return comname;
+	}
+	public void setComname(String comname) {
+		this.comname = comname;
+	}
+	public String getBillemail() {
+		return billemail;
+	}
+	public void setBillemail(String billemail) {
+		this.billemail = billemail;
+	}
+	public String getBillphone() {
+		return billphone;
+	}
+	public void setBillphone(String billphone) {
+		this.billphone = billphone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	
+	
+	
+	
+	
+}
